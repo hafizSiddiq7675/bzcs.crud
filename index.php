c5c800fc868182e2b35d49b0ac4b3ed27e110f45
hafizSiddiq7675 <hafiz.siddiq2k17@gmail.com>
2019-08-20 00:30:30 +0500

    Setup complete

---
+++ index.php
@@ -0,0 +1,3 @@
+<?php
+require_once 'site/private/initialize.php';
+header('Location: site/');
---
+++ site/private/shared/header.php
@@ -0,0 +1,15 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Web Development Project</title>
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="_css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="_css/sticky-footer-navbar.css" rel="stylesheet">
+  </head>
